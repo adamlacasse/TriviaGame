057b82c3889d9ce43b6dd7d3a979566e4edbab99
Adam LaCasse <adamlacasse@AdamSurfaceBook.(none)>
2018-04-09 18:20:13 -0400

    more styling; started JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,10 +8,66 @@ $(document).ready(function(){
         loadQuestion();        
     });
 
+var questions = [
+    {   question:"Which singer's real name is Stefani Joanne Angelina Germanotta?",
+        correctAnswer:"Lady Gaga",
+        choices: ["Madonna","Gwen Stefani","Lady Gaga","Sufjan Stevens"]
+    },
+    {   question:"The average human body contains how many pints of blood?",
+    correctAnswer:"9",
+    choices: ["9","12","5","20"]
+    },
+    {   question:"According to Greek mythology who was the first woman on earth?",
+    correctAnswer:"Pandora",
+    choices: ["Aphrodite","Daphne","Persephone","Pandora"]
+    },
+    {   question:"",
+    correctAnswer:"",
+    choices: ["","","",""]
+    },
+    {   question:"",
+    correctAnswer:"",
+    choices: ["","","",""]
+    },
+    {   question:"",
+    correctAnswer:"",
+    choices: ["","","",""]
+    },
+    {   question:"",
+    correctAnswer:"",
+    choices: ["","","",""]
+    },
+    {   question:"",
+    correctAnswer:"",
+    choices: ["","","",""]
+    },
+    {   question:"",
+    correctAnswer:"",
+    choices: ["","","",""]
+    },
+    {   question:"",
+    correctAnswer:"",
+    choices: ["","","",""]
+    }
+]
+
+var currentQuestion = 0;
+var correct = 0;
+var incorrect =  0;
+var answered = 0;
+var unanswered = 11;
+
+
 function loadQuestion(){
     $("#timer").show();
-    $("#questionSection").append("This is where the question displays");
     $("#answersSection").show();
-    }
+    // for(i=0; i < questions.length; i++){
+    // }
+    $("#questionSection").append("This is where the question displays");
+
+    } // end of loadQuestion()
+
+
+
 
 }); // end of document.ready
