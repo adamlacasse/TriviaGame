415122bec107d421421e043a42bb8db46cebf4f8
Adam LaCasse <adamlacasse@AdamSurfaceBook.(none)>
2018-04-11 15:33:13 -0400

    Added Game Over page after final click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,44 +16,44 @@ $(document).ready(function(){
 var questions = [
     {   question:"Which singer's real name is Stefani Joanne Angelina Germanotta?",
         correctAnswer:"Lady Gaga",
-        choices: ["Madonna","Gwen Stefani","Lady Gaga","Sufjan Stevens"]
-    },
-    {   question:"The average human body contains how many pints of blood?",
-    correctAnswer:"9",
-    choices: ["9","12","5","20"]
-    },
-    {   question:"According to Greek mythology who was the first woman on earth?",
-    correctAnswer:"Pandora",
-    choices: ["Aphrodite","Daphne","Persephone","Pandora"]
-    },
-    {   question:"In which country would you find the Ian Fleming International Airport, named after the James Bond author?",
-    correctAnswer:"Jamaica",
-    choices: ["England","Jamaica","United States","Denmark"]
-    },
-    {   question:"From which language does the word ‘futon’ originate?",
-    correctAnswer:"Japanese",
-    choices: ["Japanese","Chinese","Dutch","None, it's a made up word"]
-    },
-    {   question:"In the year 1900 what were the most popular baby names in the US?",
-    correctAnswer:"John and Mary",
-    choices: ["John and Mary","Joseph and Catherine","George and Anne"," William and Elizabeth"]
-    },
-    {   question:"Which of the following items was owned by the fewest US homes in 1990?",
-    correctAnswer:"compact disk player",
-    choices: ["home computer","dishwasher","cordless phone","compact disk player"]
-    },
-    {   question:"Who was the first black American pictured on a US postage stamp?",
-    correctAnswer:"Joe Louis",
-    choices: ["Joe Louis","Booker T. Washington","Louis Armstrong","Frederick Douglass"]
-    },
-    {   question:"Which album features the song 'Come Together'?",
-    correctAnswer:"Abbey Road",
-    choices: ["Abbey Road","Revolver","Help!","Rubber Soul"]
-    },
-    {   question:"Which of these songs was written by the band Aerosmith?",
-    correctAnswer:"Dream On",
-    choices: ["Whole Lotta Love","Rumours","Uptown Funk","Dream On"]
-    }
+        choices: ["Madonna","Gwen Stefani","Lady Gaga","Sufjan Stevens"] } // DON'T FORGET TO GET RID OF THIS "}"
+    // },
+    // {   question:"The average human body contains how many pints of blood?",
+    // correctAnswer:"9",
+    // choices: ["9","12","5","20"]
+    // },
+    // {   question:"According to Greek mythology who was the first woman on earth?",
+    // correctAnswer:"Pandora",
+    // choices: ["Aphrodite","Daphne","Persephone","Pandora"]
+    // },
+    // {   question:"In which country would you find the Ian Fleming International Airport, named after the James Bond author?",
+    // correctAnswer:"Jamaica",
+    // choices: ["England","Jamaica","United States","Denmark"]
+    // },
+    // {   question:"From which language does the word ‘futon’ originate?",
+    // correctAnswer:"Japanese",
+    // choices: ["Japanese","Chinese","Dutch","None, it's a made up word"]
+    // },
+    // {   question:"In the year 1900 what were the most popular baby names in the US?",
+    // correctAnswer:"John and Mary",
+    // choices: ["John and Mary","Joseph and Catherine","George and Anne"," William and Elizabeth"]
+    // },
+    // {   question:"Which of the following items was owned by the fewest US homes in 1990?",
+    // correctAnswer:"compact disk player",
+    // choices: ["home computer","dishwasher","cordless phone","compact disk player"]
+    // },
+    // {   question:"Who was the first black American pictured on a US postage stamp?",
+    // correctAnswer:"Joe Louis",
+    // choices: ["Joe Louis","Booker T. Washington","Louis Armstrong","Frederick Douglass"]
+    // },
+    // {   question:"Which album features the song 'Come Together'?",
+    // correctAnswer:"Abbey Road",
+    // choices: ["Abbey Road","Revolver","Help!","Rubber Soul"]
+    // },
+    // {   question:"Which of these songs was written by the band Aerosmith?",
+    // correctAnswer:"Dream On",
+    // choices: ["Whole Lotta Love","Rumours","Uptown Funk","Dream On"]
+    // }
 ]
 
 var currentQuestion = 0;
@@ -61,7 +61,6 @@ var correct = 0;
 var incorrect =  0;
 var answered = 0;
 var unanswered = 11;
-
 
 function loadQuestion(){
     $("#nextQuestion").hide();
@@ -109,13 +108,12 @@ $("#butA4").on("click",function(){
     } else incorrectAnswer();
 });
 
-
 function correctAnswer(){
     $("#answersSection").hide();
     $("#timer").hide();
     $("#winSection").show();
     correct++;
-    if(correct + incorrect < 10){
+    if(correct + incorrect < questions.length){
         $("#nextQuestion").show();
         } else {$("#seeResults").show(); $("#nextQuestion").hide();}
 }
@@ -126,7 +124,7 @@ function incorrectAnswer(){
     $("#loseSection").show();
     incorrect++;
     $("#nextQuestion").show();
-    if(correct + incorrect < 10){
+    if(correct + incorrect < questions.length){
         $("#nextQuestion").show();
         } else {$("#seeResults").show(); $("#nextQuestion").hide();}
 }
@@ -134,6 +132,10 @@ function incorrectAnswer(){
 $("#nextQuestion").on("click",function(){
     currentQuestion++;    
     loadQuestion();
+});
+
+$("#seeResults").on("click",function(){
+    $("#game-action").html("<img id='lose' src ='./assets/images/theEnd.jpg'></img>");
 });
 
 
