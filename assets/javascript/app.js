42baddfbd29a37d177a9db8dd7159754d279ca87
Adam LaCasse <adamlacasse@AdamSurfaceBook.(none)>
2018-04-12 11:30:51 -0400

    very close with timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -82,10 +82,13 @@ function loadQuestion(){
 
     var timeleft = 10;
     var downloadTimer = setInterval(function(){
-                            document.getElementById("progressBar").value = 10 - --timeleft;
-                            if(timeleft <= 0)
-                            clearInterval(downloadTimer);
-                            },1000);
+    timeleft--;
+    document.getElementById("timer").textContent = timeleft;
+    if(timeleft <= 0){
+        clearInterval(downloadTimer);
+        timeOut();
+        }
+    },1000);
 
 } // end of loadQuestion()
 
